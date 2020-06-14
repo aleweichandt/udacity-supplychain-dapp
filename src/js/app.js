6134578d5178e09fa95163cb2307c7590452c802
alew <ale.w.es@sync.money>
2020-06-14 13:57:49 +0200

    app running

--- src/js/app.js
+++ src/js/app.js
@@ -74,15 +74,15 @@ App = {
         }
         // If no injected web3 instance is detected, fall back to Ganache
         else {
-            App.web3Provider = new Web3.providers.HttpProvider('http://localhost:7545');
+            App.web3Provider = new Web3.providers.HttpProvider('http://localhost:8545');
         }
 
-        App.getMetaskAccountID();
+        await App.getMetaskAccountID();
 
         return App.initSupplyChain();
     },
 
-    getMetaskAccountID: function () {
+    getMetaskAccountID: async function () {
         web3 = new Web3(App.web3Provider);
 
         // Retrieving accounts
